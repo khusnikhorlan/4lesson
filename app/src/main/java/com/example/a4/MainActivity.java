807a437877a21565d91865b4abe617a23f277ae1
khusnikhorlan <khusni.khorlan@gmail.com>
2020-12-06 17:32:33 +0600

    4lesson

---
+++ app/src/main/java/com/example/a4/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.a4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        MenuInflater menuInflter = getMenuInflater();
+        menuInflter.inflate(R.layout.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menusettings:
+                Toast.makeText(this, "Settings menu clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menudelete:
+                Toast.makeText(this, "Delete menu clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuupdate:
+                Toast.makeText(this, "Update menu clicked", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+}
